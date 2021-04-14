090af019698780cff85fb4f9e0cffec1f9a44fb5
AL1525 <AL1525@users.noreply.github.com>
2021-04-13 22:18:36 -0400

    base?

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 console.log("connected");
-import { questions } from "./question.js";
 
 (function () {
   // Functions
@@ -100,7 +99,108 @@ import { questions } from "./question.js";
   const quizContainer = document.getElementById("quiz");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
-  const myQuestions = [questions];
+  const myQuestions = [
+    {
+      question: "Question 1",
+      answers: {
+        a: "Choice 1",
+        b: "Choice 2",
+        c: "Choice 3",
+        d: "Choice 4",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 2",
+      answers: {
+        a: "Choice 5",
+        b: "Choice 6",
+        c: "Choice 7",
+        d: "Choice 8",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 3",
+      answers: {
+        a: "Choice 9",
+        b: "Choice 10",
+        c: "Choice 11",
+        d: "Choice 12",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 4",
+      answers: {
+        a: "Choice 13",
+        b: "Choice 14",
+        c: "Choice 15",
+        d: "Choice 16",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 5",
+      answers: {
+        a: "Choice 17",
+        b: "Choice 18",
+        c: "Choice 19",
+        d: "Choice 20",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 6",
+      answers: {
+        a: "Choice 21",
+        b: "Choice 22",
+        c: "Choice 23",
+        d: "Choice 24",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 7",
+      answers: {
+        a: "Choice 25",
+        b: "Choice 26",
+        c: "Choice 27",
+        d: "Choice 28",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 8",
+      answers: {
+        a: "Choice 29",
+        b: "Choice 30",
+        c: "Choice 31",
+        d: "Choice 32",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 9",
+      answers: {
+        a: "Choice 33",
+        b: "Choice 34",
+        c: "Choice 35",
+        d: "Choice 36",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Question 10",
+      answers: {
+        a: "Choice 37",
+        b: "Choice 38",
+        c: "Choice 39",
+        d: "Choice 40",
+      },
+      correctAnswer: "c",
+    },
+  ];
 
   // Kick things off
   buildQuiz();
